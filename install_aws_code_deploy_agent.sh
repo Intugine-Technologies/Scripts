c277e7682dab285edd70f4d7d82a8e34de1459e9
intugine <38681621+intugine@users.noreply.github.com>
2020-05-27 13:52:30 +0530

    Update install_aws_code_deploy_agent.sh

--- install_aws_code_deploy_agent.sh
+++ install_aws_code_deploy_agent.sh
@@ -3,7 +3,7 @@ echo "[Start] Installation of AWS CodDeploy Agent"
 apt-get -y update
 apt-get install -y ruby
 # cd /tmp
-wget -o code_deploy.sh https://aws-codedeploy-ap-south-1.s3.amazonaws.com/latest/install;
+wget -O code_deploy.sh https://aws-codedeploy-ap-south-1.s3.amazonaws.com/latest/install;
 chmod +x ./code_deploy.sh
 ./code_deploy.sh auto
 service codedeploy-agent start
